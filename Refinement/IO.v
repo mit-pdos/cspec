07119c2cb37449fe76aa8cf759736f99888dd955
Tej Chajed <tchajed@mit.edu>
2017-04-26 19:42:22 -0400

    Nicely comment the IO monad definition
    Well, everything but explaining what a monad is.

--- Refinement/IO.v
+++ Refinement/IO.v
@@ -2,24 +2,51 @@
 
 Require Import Automation.
 
+(* We use Coq's implicit arguments feature, where arguments that can be inferred
+(for example, types for polymorphic code) do not have to be passed. This option
+causes Coq to use some heuristics to automatically make some arguments to
+definitions implicit. *)
 Global Set Implicit Arguments.
 
-Axiom IO : Type -> Type.
+(* [Axiom IO] assumes the existence of programs; an [IO T] should be thought of
+as a computation that has some side-effect and then returns a value of type T.
+We will assume programs have a semantics (in terms of more opaque things) and
+turn IO into a monad here.
+
+Later on we will assume some more primitive [IO T]'s that manipulate disks,
+finally using these to write programs that manipulate IO with assumptions about
+how the primitives behave.
+*)
+
+Axiom IO : forall (T:Type), Type.
+
 Axiom Ret : forall T, T -> IO T.
 Axiom Bind : forall T T', IO T -> (T -> IO T') -> IO T'.
 
-(* state the IO monad manipulates *)
+(* The opaque state that all code in the IO monad manipulates. *)
 Axiom world : Type.
 
-Arguments IO T.
-Arguments Ret {T} v.
-Arguments Bind {T T'} io io'.
-
+(* io_step p w v w' holds when p can step from w to w' and return v *)
 Axiom io_step : forall T, IO T -> world -> T -> world -> Prop.
+
+(** Now we provide the basic monadic combinators in IO and assume that io_step
+gives them the desired semantics. *)
+
+(* We have to adjust the automatically determined implicit arguments for Ret and
+Bind to match what we'd like, and also set the argument names. *)
+Arguments Ret {T} v.
+Arguments Bind {T T'} p p'.
 
 Axiom ret_step : forall T (v:T) w v' w',
     io_step (Ret v) w v' w' <->
     v' = v /\ w' = w.
+
+Axiom bind_step : forall T T' (p: IO T) (p': T -> IO T') w v' w'',
+    io_step (Bind p p') w v' w'' <->
+    (exists v w', io_step p w v w' /\
+             io_step (p' v) w' v' w'').
+
+(* For convenience we re-state one direction of the combinator semantics. *)
 
 Lemma ret_does_step : forall T (v:T) w,
     io_step (Ret v) w v w.
@@ -27,11 +54,6 @@ Proof.
   intros.
   apply ret_step; eauto.
 Qed.
-
-Axiom bind_step : forall T T' (p: IO T) (p': T -> IO T') w v' w'',
-    io_step (Bind p p') w v' w'' <->
-    (exists v w', io_step p w v w' /\
-             io_step (p' v) w' v' w'').
 
 Lemma bind_does_step : forall T T' (p: IO T) (p': T -> IO T') w v w' v' w'',
     io_step p w v w' ->
@@ -41,6 +63,10 @@ Proof.
   intros.
   eapply bind_step; eauto.
 Qed.
+
+(* As a consequence of the semantics we've given above, io_step respects the
+monad equivalences: for example, Bind p Ret should be equivalent to p, so we
+prove that they have the same io_step rules (monad_right_id). *)
 
 Definition io_equiv T (step1 step2: world -> T -> world -> Prop) :=
   forall w v w', step1 w v w' <-> step2 w v w'.
