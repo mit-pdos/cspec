998167b0a95dc53e1ce7b2e98f71d0c81c4cb3be
Tej Chajed <tchajed@mit.edu>
2017-04-26 10:55:39 -0400

    Change sigma to pstate in a few more places

--- ProgTheorems.v
+++ ProgTheorems.v
@@ -7,34 +7,34 @@ Set Implicit Arguments.
 
 Hint Constructors exec.
 
-Theorem can_crash_at_begin : forall T (p: prog3 T) sigma,
-    exec p sigma (Crashed sigma).
+Theorem can_crash_at_begin : forall T (p: prog3 T) pstate,
+    exec p pstate (Crashed pstate).
 Proof.
   eauto.
 Qed.
 
-Theorem can_crash_at_end : forall T (p: prog3 T) sigma v sigma',
-    exec p sigma (Finished v sigma') ->
-    exec p sigma (Crashed sigma').
+Theorem can_crash_at_end : forall T (p: prog3 T) pstate v pstate',
+    exec p pstate (Finished v pstate') ->
+    exec p pstate (Crashed pstate').
 Proof.
   induction p; intros;
     inv_exec;
     eauto.
 Qed.
 
-Theorem exec_decidable T (p: prog3 T) sigma :
-  { r | let '(v, sigma') := r in
-        exec p sigma (Finished v sigma')} + {exec p sigma Failed}.
+Theorem exec_decidable T (p: prog3 T) pstate :
+  { r | let '(v, pstate') := r in
+        exec p pstate (Finished v pstate')} + {exec p pstate Failed}.
 Proof.
-  generalize dependent sigma.
+  generalize dependent pstate.
   induction p; simpl; intros.
-  - destruct (disk_id i sigma a) eqn:?.
+  - destruct (disk_id i pstate a) eqn:?.
     + left.
       refine (exist _ (_, _) _).
       eapply ExecStepTo; simpl; simpl_match; eauto.
     + right.
       eapply ExecStepFail; simpl; simpl_match; eauto.
-  - destruct (disk_id i sigma a) eqn:?.
+  - destruct (disk_id i pstate a) eqn:?.
     + left.
       refine (exist _ (_, _) _).
       eapply ExecStepTo; simpl; simpl_match; eauto.
@@ -43,12 +43,12 @@ Proof.
   - left.
     refine (exist _ (_, _) _).
     eapply ExecStepTo; simpl; eauto.
-  - destruct (IHp sigma); eauto.
-    destruct s as [ [v sigma'] ?].
+  - destruct (IHp pstate); eauto.
+    destruct s as [ [v pstate'] ?].
     specialize (X v).
-    destruct (X sigma'); eauto.
-    destruct s as [ [v' sigma''] ?].
-    left; exists (v', sigma'').
+    destruct (X pstate'); eauto.
+    destruct s as [ [v' pstate''] ?].
+    left; exists (v', pstate'').
     eauto.
 Defined.
 
@@ -60,13 +60,13 @@ between programs).
  *)
 
 Definition exec_equiv T (p p': prog3 T) :=
-  forall sigma r, exec p sigma r <-> exec p' sigma r.
+  forall pstate r, exec p pstate r <-> exec p' pstate r.
 
-Lemma exec_ret : forall T (v:T) sigma r,
-    exec (Ret v) sigma r ->
+Lemma exec_ret : forall T (v:T) pstate r,
+    exec (Ret v) pstate r ->
     match r with
-    | Finished v' sigma' => v = v' /\ sigma = sigma'
-    | Crashed sigma' => sigma = sigma'
+    | Finished v' pstate' => v = v' /\ pstate = pstate'
+    | Crashed pstate' => pstate = pstate'
     | Failed => False
     end.
 Proof.
@@ -121,5 +121,5 @@ Proof.
 Qed.
 
 (* Local Variables: *)
-(* company-coq-local-symbols: (("Sigma" . ?Σ) ("sigma" . ?σ) ("sigma'" . (?σ (Br . Bl) ?'))) *)
+(* company-coq-local-symbols: (("Pstate" . ?Σ) ("pstate" . ?σ) ("pstate'" . (?σ (Br . Bl) ?'))) *)
 (* End: *)
