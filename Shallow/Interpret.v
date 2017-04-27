f5dce327be976de961099ac0a95279349a7f907d
Tej Chajed <tchajed@mit.edu>
2017-04-27 15:46:57 -0400

    Add crashes to interpreter correctness
    Using the same invariant for crashes is definitely not general enough,
    but let's get through some simpler stuff before constructing an example
    where we need a bonafide crash invariant.

--- Shallow/Interpret.v
+++ Shallow/Interpret.v
@@ -30,35 +30,75 @@ Section Interpreter.
   Notation exec1 := (exec step1).
   Notation exec2 := (exec step2).
 
+  (* TODO: in general, it might not be the case that crash states line up with
+  abstract states as desired; there might be states that are only exposed by
+  recovery and that are then patched up during recovery. *)
+  Definition res_abstraction T (r: Result State1 T) : Result State2 T :=
+    match r with
+    | Finished v state => Finished v (abstraction state)
+    | Crashed state => Crashed (abstraction state)
+    end.
+
+  (* TODO: similar problem here, where there might be a weaker crash
+  invariant *)
+  Definition res_invariant T (r: Result State1 T) : Prop :=
+    match r with
+    | Finished _ state => invariant state
+    | Crashed state => invariant state
+    end.
+
   Hint Constructors exec.
 
   Theorem interpret_exec :
-    (forall T (op: opT2 T) state v state',
-        invariant state ->
-        exec1 (op_impl op) state (Finished v state') ->
-        step2 op (abstraction state) v (abstraction state') /\ invariant state') ->
-    forall T (p: prog2 T) state v state',
+    (* every operation is implemented according to the step semantics of the
+    spec language *)
+    forall (Hop_ok: forall T (op: opT2 T) state v state',
+          invariant state ->
+          exec1 (op_impl op) state (Finished v state') ->
+          step2 op (abstraction state) v (abstraction state') /\ invariant state')
+      (* every implementation crashes in states that are abstractly either the
+        pre state or the post state
+
+        TODO: this is too strong, we only need this property after a crash
+        transform and recovery; the intermediate states can satisfy some weaker
+        crash invariant
+       *)
+      (Hop_crash: forall T (op: opT2 T) state state',
+          invariant state ->
+          exec1 (op_impl op) state (Crashed state') ->
+          (abstraction state' = abstraction state \/
+           exists v, step2 op (abstraction state) v (abstraction state')) /\
+          invariant state'),
+    forall T (p: prog2 T) state r,
       invariant state ->
-      exec1 (interpret p) state (Finished v state') ->
-      exec2 p (abstraction state) (Finished v (abstraction state')) /\
-      invariant state'.
+      exec1 (interpret p) state r ->
+      exec2 p (abstraction state) (res_abstraction r) /\
+      res_invariant r.
   Proof.
     induction p; simpl; intros.
-    eapply H in H1; eauto.
-    intuition eauto.
-
-    inv_exec; eauto.
-
-    inv_exec.
-    match goal with
-    | [ Hexec: exec1 (interpret p) _ _ |- _ ] =>
-      eapply IHp in Hexec; eauto; safe_intuition
-    end.
-    match goal with
-    | [ Hexec: exec1 (interpret (p0 _)) _ _ |- _ ] =>
-      eapply H0 in Hexec; eauto; safe_intuition
-    end.
-    intuition eauto.
+    - destruct r; simpl.
+      + eapply Hop_ok in H0; eauto.
+        intuition eauto.
+      + eapply Hop_crash in H0; intuition eauto;
+          repeat deex;
+          eauto.
+        replace (abstraction state0); eauto.
+    - inv_exec; simpl; eauto.
+    - inv_exec.
+      + match goal with
+        | [ Hexec: exec1 (interpret p) _ _ |- _ ] =>
+          eapply IHp in Hexec; simpl in *; eauto; safe_intuition
+        end.
+        match goal with
+        | [ Hexec: exec1 (interpret (p0 _)) _ _ |- _ ] =>
+          eapply H in Hexec; eauto; safe_intuition
+        end.
+        intuition eauto.
+      + match goal with
+        | [ Hexec: exec1 (interpret p) _ _ |- _ ] =>
+          eapply IHp in Hexec; simpl in *; eauto; safe_intuition
+        end.
+        intuition eauto.
   Qed.
 
 End Interpreter.
