26bb5e7360dc42fcc7d4c48d80ce8793c1667de0
Tej Chajed <tchajed@mit.edu>
2017-04-30 18:43:00 -0400

    Implement an unproven RD recovery procedure
    This is actually somewhat tricky; the proof might be reasonably
    involved. Doesn't help that it's a loop.

--- Shallow/ReplicatedDisk.v
+++ Shallow/ReplicatedDisk.v
@@ -28,6 +28,54 @@ Module RD.
       _ <- Prim (TD.Write d1 a b);
       Ret tt.
 
+  (* Recovery tracks what happens at each step in order to implement control
+  flow. *)
+  Inductive RecStatus :=
+  (* continue working, nothing interesting has happened *)
+  | Continue
+  (* some address has been repaired (or the recovery has exhausted the
+  addresses) - only one address can be out of sync and thus only it must be
+  recovered. *)
+  | RepairDone
+  (* one of the disks has failed, so don't bother continuing recovery since the
+  invariant is now trivially satisfied *)
+  | DiskFailed.
+
+  Definition fixup (a:addr) : TD.prog RecStatus :=
+    mv0 <- Prim (TD.Read d0 a);
+      match mv0 with
+      | Working v => mv2 <- Prim (TD.Read d1 a);
+                      match mv2 with
+                      | Working v' => if v == v' then
+                                       Ret Continue
+                                     else
+                                       mu <- Prim (TD.Write d1 a v);
+                                       Ret (match mu with
+                                            | Working _ => RepairDone
+                                            | Failed => DiskFailed
+                                            end)
+                      | Failed => Ret DiskFailed
+                      end
+      | Failed => Ret DiskFailed
+      end.
+
+  (* recursively performs recovery at [a-1], [a-2], down to 0 *)
+  Fixpoint recover_at (a:addr) : TD.prog RecStatus :=
+    match a with
+    | 0 => Ret RepairDone
+    | S n => s <- fixup n;
+              match s with
+              | Continue => recover_at n
+              | RepairDone => Ret RepairDone
+              | DiskFailed => Ret DiskFailed
+              end
+    end.
+
+  (* recovery for a disk of size [sz] *)
+  Definition Recover (sz:nat) : TD.prog unit :=
+    _ <- recover_at sz;
+      Ret tt.
+
   Definition op_impl T (op:D.Op T) : TD.prog T :=
     match op with
     | D.Read a => Read a
