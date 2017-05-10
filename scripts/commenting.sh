59e52fd6a4b3c681f752cd6277c4e27f7b6e0c53
Tej Chajed <tchajed@mit.edu>
2017-05-10 08:44:04 -0400

    Add script to how well-commented code is

---
+++ scripts/commenting.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+set -e
+
+# Print statistics about how well code is commented
+
+tmp=$(mktemp -t "cloc.sql")
+
+cloc --quiet --vcs=git -sql="$tmp"
+sqlite3 -cmd ".read \"$tmp\"" -cmd '.headers on' -cmd '.width 38' -column <<EOF
+select File, nCode+nComment as "code lines",
+	round(cast(nComment as float)/(nCode+nComment), 2) as "% comments"
+	from t
+	where Language = "Coq"
+	order by "% comments" asc, "code lines" desc;
+
+.print ""
+
+select sum(nCode+nComment) as "total lines",
+	sum(nComment) as "total comments",
+	round(sum(cast(nComment as float))/sum(nCode+nComment), 2) as "% comments"
+	from t
+	where Language = "Coq";
+EOF
+
+rm -f "$tmp"
