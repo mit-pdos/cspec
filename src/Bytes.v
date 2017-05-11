0fc7ce32ea4cc7a81cde0d8bad658b2482d34752
Tej Chajed <tchajed@mit.edu>
2017-05-11 18:18:20 -0400

    Simplify extraction a bit

--- src/Bytes.v
+++ src/Bytes.v
@@ -7,20 +7,6 @@ Set Implicit Arguments.
 (* bytes n is a byte string with length n. *)
 Parameter bytes : nat -> Type.
 
-Parameter bytes_eqb : forall n, bytes n -> bytes n -> bool.
-
-Axiom bytes_eqb_correct : forall n (b1 b2:bytes n),
-    bytes_eqb b1 b2 = true <-> b1 = b2.
-
-Instance bytes_dec n : EqDec (bytes n) eq.
-Proof.
-  hnf; intros.
-  unfold equiv, complement.
-  destruct (bytes_eqb x y) eqn:?.
-  - left.
-    apply bytes_eqb_correct; auto.
-  - right; intro.
-    apply bytes_eqb_correct in H; congruence.
-Defined.
+Parameter bytes_dec : forall n, EqDec (bytes n) eq.
 
 Axiom bytes0 : forall n, bytes n.
--- src/Shallow/ExtractReplicatedDisk.v
+++ src/Shallow/ExtractReplicatedDisk.v
@@ -7,17 +7,18 @@ Extraction Language Haskell.
 Require Import Bytes.
 
 Extract Constant bytes => "BS.ByteString".
-Extract Constant bytes_eqb => "(\n b1 b2 -> if b1 Prelude.== b2 then Datatypes.Coq_true else Datatypes.Coq_false)".
+Extract Constant bytes_dec => "(\n b1 b2 -> if b1 Prelude.== b2 then Specif.Coq_left else Specif.Coq_right)".
 Extract Constant bytes0 => "(\n -> BS.replicate (natToInt n) 0)".
 
 (* TODO: add the following to the imports of Bytes:
 
-import CoqUtils
+import qualified Specif
 import qualified Data.ByteString as BS
+import CoqUtils
  *)
 
 Require Import ReplicatedDisk.
-Recursive Extraction Library ReplicatedDisk.
+Separate Extraction RD.Read RD.Write RD.Recover.
 
 (* this appears to be needed to compile this file *)
 Cd "../../".
