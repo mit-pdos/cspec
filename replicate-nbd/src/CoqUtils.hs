1a90384d27b7a1d1f4af018d57c95660dda2977a
Tej Chajed <tchajed@mit.edu>
2017-05-11 16:10:05 -0400

    Mostly fix compilation of extracted Haskell code

---
+++ replicate-nbd/src/CoqUtils.hs
@@ -0,0 +1,9 @@
+module CoqUtils where
+
+import qualified Datatypes
+
+natToInt :: Datatypes.Coq_nat -> Int
+natToInt n =
+  case n of
+    Datatypes.O -> 0
+    Datatypes.S n' -> 1+natToInt n'
--- src/Shallow/ExtractReplicatedDisk.v
+++ src/Shallow/ExtractReplicatedDisk.v
@@ -1,17 +1,23 @@
 (** uncomment for interactive usage *)
 (* Cd "../../". *)
-Cd "replicate-nbd/src/ReplicatedDisk/".
+Cd "replicate-nbd/src/".
 
 Extraction Language Haskell.
 
 Require Import Bytes.
 
 Extract Constant bytes => "BS.ByteString".
-Extract Constant bytes_eqb => "(=)".
-Extract Constant bytes0 => "(\n -> BS.replicate n 0)".
+Extract Constant bytes_eqb => "(\n b1 b2 -> if b1 Prelude.== b2 then Datatypes.Coq_true else Datatypes.Coq_false)".
+Extract Constant bytes0 => "(\n -> BS.replicate (natToInt n) 0)".
+
+(* TODO: add the following to the imports of Bytes:
+
+import CoqUtils
+import qualified Data.ByteString as BS
+ *)
 
 Require Import ReplicatedDisk.
 Recursive Extraction Library ReplicatedDisk.
 
 (* this appears to be needed to compile this file *)
-Cd "../../../".
+Cd "../../".
