7a3677dba748efeed67330f32b1a71ae59802a26
Tej Chajed <tchajed@mit.edu>
2017-05-12 15:58:37 -0400

    Fix imports for only Bytes module
    Don't rely on filename

--- replicate-nbd/fiximports.py
+++ replicate-nbd/fiximports.py
@@ -3,6 +3,7 @@
 # preprocessor: add extra imports
 # preprocessor: ghc -F -pgmF
 
+import re
 import os, sys
 
 imports = """
@@ -15,10 +16,17 @@ fs_filename = sys.argv[1]
 filename = sys.argv[2]
 out = open(sys.argv[3], "w")
 
+module_name = None
+
+MODULE_RE = re.compile("module (?P<module>.*) where\n")
+
 out.write("{-# LINE 1 \"%s\" #-}\n" % (filename))
 for n, line in enumerate(open(filename), 1):
+        m = MODULE_RE.match(line)
+        if m:
+            module_name = m.group("module")
 	if (line.strip() == "import qualified Prelude" and
-                fs_filename == "src/Bytes.hs"):
+                module_name == "Bytes"):
 		out.write(imports)
 		out.write("{-# LINE %d \"%s\" #-}\n" % (n, filename))
 	line = line.replace('__FILE__', '"%s"' % sys.argv[2])
