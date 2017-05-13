a92cd33fa62070afc08ea362dd588ff9aa0e15b8
Tej Chajed <tchajed@mit.edu>
2017-05-13 11:47:32 -0400

    Also keep track of Haskell comments
    This code has much further to go to being readable. It also arguably
    doesn't all have to be uniformly good - students will need to read
    little to none of it for the labs. Nonetheless the high-level structure,
    and how Coq fits in, is probably interesting for some people, and worth
    explaining.

--- scripts/commenting.sh
+++ scripts/commenting.sh
@@ -21,6 +21,22 @@ select sum(nCode+nComment) as "total lines",
 	round(sum(cast(nComment as float))/sum(nCode+nComment), 2) as "% comments"
 	from t
 	where Language = "Coq";
+
+.print ""
+
+select File, nCode+nComment as "code lines",
+	round(cast(nComment as float)/(nCode+nComment), 2) as "% comments"
+	from t
+	where Language = "Haskell"
+	order by "% comments" asc, "code lines" desc;
+
+.print ""
+
+select sum(nCode+nComment) as "total Haskell lines",
+	sum(nComment) as "total comments",
+	round(sum(cast(nComment as float))/sum(nCode+nComment), 2) as "% comments"
+	from t
+	where Language = "Haskell";
 EOF
 
 rm -f "$tmp"
