e786e3a2f8c52073922d2b3ff7c255dc483646ef
Tej Chajed <tchajed@mit.edu>
2017-05-13 10:51:02 -0400

    Add basic option parsing
    Need to figure out how to get the disks to be optional in a nice way
    (want to allow 0 or two arguments, nothing else).

--- replicate-nbd/app/Main.hs
+++ replicate-nbd/app/Main.hs
@@ -1,27 +1,42 @@
 module Main where
 
 import Control.Monad (unless, forM_)
+import Data.Semigroup ((<>))
 import Network.NBD (runServer)
+import Options.Applicative
 import System.Directory
 import System.IO
 
--- TODO: need to support argument/option parsing for file names
-fn0 :: FilePath
-fn0 = "disk0.img"
+data Options = Options
+  { defaultSizeKB :: Int
+  , disk0Path :: FilePath
+  , disk1Path :: FilePath }
 
-fn1 :: FilePath
-fn1 = "disk1.img"
-
--- if creating empty disk0.img, disk1.img, size to set in bytes
-size :: Integer
-size = 100*1024*1024
+options :: Parser Options
+options = Options
+  <$> option auto
+    ( long "size"
+      <> help "size to initialize disk files to if they do not exist"
+      <> showDefault
+      <> value (100*1024)
+      <> metavar "KB" )
+  <*> argument str (metavar "DISK0")
+  <*> argument str (metavar "DISK1")
 
 main :: IO ()
-main = do
+main = execParser opts >>= run
+  where
+    opts = info (options <**> helper)
+      (fullDesc
+       <> progDesc "start an nbd server that replicates over two disks"
+       <> header "replicate-nbd - replicating network block device")
+
+run :: Options -> IO ()
+run Options {defaultSizeKB=size, disk0Path=fn0, disk1Path=fn1} = do
   exists0 <- doesFileExist fn0
   exists1 <- doesFileExist fn1
   unless (exists0 && exists1) $
     forM_ [fn0, fn1] $ \p ->
       withFile p WriteMode $ \h ->
-        hSetFileSize h size
+        hSetFileSize h (fromIntegral $ size * 1024)
   runServer fn0 fn1
