04ee52ab4ed0f82159308f63da5b164892d58f1e
Tej Chajed <tchajed@mit.edu>
2017-05-13 07:33:25 -0400

    Move axiomatic Byte extraction to separate file

--- ExtractReplicatedDisk.v
+++ ExtractReplicatedDisk.v
@@ -1,20 +1,9 @@
 Cd "replicate-nbd/src/".
 
-Extraction Language Haskell.
-
 Require Import ExtrHaskellNatInteger.
-Require Import Bytes.
+Require Import ExtrBytes.
 
-Extract Constant bytes => "BS.ByteString".
-Extract Constant bytes_dec => "(\n b1 b2 -> if b1 Prelude.== b2 then Specif.Coq_left else Specif.Coq_right)".
-Extract Constant bytes0 => "(\n -> BS.replicate (Prelude.fromIntegral n) 0)".
-
-(* TODO: add the following to the imports of Bytes:
-
-import qualified Specif
-import qualified Data.ByteString as BS
-import CoqUtils
- *)
+Extraction Language Haskell.
 
 Require Import ReplicatedDisk.
 Separate Extraction RD.Read RD.Write RD.Recover.
