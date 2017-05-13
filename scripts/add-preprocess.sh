1e02c6ec6a6b6b140825f043e788a6ed7f35744a
Tej Chajed <tchajed@mit.edu>
2017-05-13 09:57:02 -0400

    Make sed work cross-platform

--- scripts/add-preprocess.sh
+++ scripts/add-preprocess.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
+set -e
 
-sed -i '' $'1s|^|{-# OPTIONS_GHC -F -pgmF ./fiximports.py #-}\\\n|' "$1"
+# cross-platform sed (because OS X only has BSD sed):
+# see http://stackoverflow.com/questions/5694228/sed-in-place-flag-that-works-both-on-mac-bsd-and-linux
+
+sed -i.bak $'1s|^|{-# OPTIONS_GHC -F -pgmF ./fiximports.py #-}\\\n|' "$1"
+rm "$1.bak"
