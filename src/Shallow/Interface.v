2a735c711136dfc47cbbce7d92e9500091f6c3a4
Tej Chajed <tchajed@mit.edu>
2017-05-16 11:28:10 -0400

    Move to only having rspecs for an interface
    This should actually be doable - need principles to handle recovery
    specs better (esp a prog_rok would be nice), but should be able to prove
    things with recovery just being the lower-level recovery (which are like
    crash specs) and then separately prove that the recovery procedure
    tacked on to the end does the right thing.
    
    Open question is how crash relations will work, since they now have to
    be handled everywhere. Crash transformers should help, but have to work
    out some details.

--- src/Shallow/Interface.v
+++ src/Shallow/Interface.v
@@ -1,5 +1,8 @@
+Require Import Automation.
+
 Require Import ProgLang.Prog.
 Require Import ProgLang.Hoare.
+Require Import ProgLang.HoareRecovery.
 
 Record InterfaceAPI opT State :=
   { op_sem: forall T, opT T -> Semantics State T; }.
@@ -10,34 +13,31 @@ Definition background_step {opT State} (bg_step: State -> State -> Prop)
                  exists state', bg_step state state' /\
                        step op state' v state''; |}.
 
-Record InterfaceImpl opT :=
-  { op_impl: forall T, opT T -> prog T;
-    (* TODO: add a recovery procedure here *) }.
-
-(* TODO: as with Interpret, this crash spec is nice and all but we really want a
-recovery specification (even for the replicated disk). This also op_spec will
-need to depend on both InterfaceAPI and InterfaceImpl (unfortunately). *)
-
-Definition op_spec opT `(api: InterfaceAPI opT State) `(op: opT T) : Specification unit T State :=
+Definition op_spec opT `(api: InterfaceAPI opT State) `(op: opT T) : RecSpecification unit T unit State :=
   fun (_:unit) state =>
     {|
-      pre := True;
-      post :=
+      rec_pre := True;
+      rec_post :=
         fun v state' => op_sem api op state v state';
-      crash :=
-        fun state' =>
-          state' = state;
+      recover_post :=
+        fun r state' =>
+          r = tt /\
+          (state' = state \/
+           exists v, op_sem api op state v state');
     |}.
+
+Record InterfaceImpl opT :=
+  { op_impl: forall T, opT T -> prog T;
+    recover_impl: prog unit; }.
 
 Record Interface opT State (api: InterfaceAPI opT State) :=
   { interface_impl: InterfaceImpl opT;
     refinement: Refinement State;
     impl_ok : forall `(op: opT T),
-        (* TODO: this atomicity spec is too hard to provide, should prove a
-        prog_rok wrt the implementation recovery procedure *)
-        prog_ok (op_spec api op)
-                (op_impl interface_impl _ op)
-                refinement; }.
+        prog_rspec (op_spec api op)
+                   (op_impl interface_impl _ op)
+                   (recover_impl interface_impl)
+                   refinement; }.
 
 Definition Prim opT `{api: InterfaceAPI opT State}
            (i:Interface api)
@@ -52,19 +52,22 @@ Add Printing Coercion Prim.
 Theorem prim_ok : forall opT `(api: InterfaceAPI opT State)
                     `(i: Interface api)
                     `(op: opT T)
-                    `(spec: Specification A T State),
-    (forall a state, pre (spec a state) ->
+                    `(spec: RecSpecification A T unit State),
+    (forall a state, rec_pre (spec a state) ->
             forall v state', op_sem api op state v state' ->
-                    post (spec a state) v state') ->
-    (forall a state, pre (spec a state) ->
-            crash (spec a state) state) ->
-    prog_ok spec (Prim i op) (refinement i).
+                    rec_post (spec a state) v state') ->
+    (forall a state, rec_pre (spec a state) ->
+            recover_post (spec a state) tt state) ->
+    (forall a state, rec_pre (spec a state) ->
+            forall v state', rec_post (spec a state) v state' ->
+                    recover_post (spec a state) tt state') ->
+    prog_rspec spec (Prim i op) (recover_impl (interface_impl i)) (refinement i).
 Proof.
   intros.
-  eapply prog_ok_weaken; [ eapply (impl_ok i) | ].
-  unfold spec_impl; intros.
-  exists tt; repeat match goal with
-               | [ |- _ /\ _ ] => split
-               end; simpl; eauto.
-  intuition (subst; eauto); eauto.
+  eapply prog_rspec_weaken.
+  eapply (impl_ok i).
+  unfold rspec_impl; intros.
+  exists tt; simpl; intuition.
+  subst; eauto.
+  repeat deex; eauto.
 Qed.
