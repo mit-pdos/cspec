f8cb633e38589e0fbe4fd7fc5f140fc3d3ad28e2
Tej Chajed <tchajed@mit.edu>
2017-05-15 21:00:06 -0400

    Fix SeqDiskAPI

--- src/Shallow/SeqDiskAPI.v
+++ src/Shallow/SeqDiskAPI.v
@@ -12,11 +12,9 @@ Module D.
   | Write (a:addr) (b:block) : Op unit
   | DiskSize : Op nat.
 
-  Definition prog := Prog.prog Op.
-
   Definition State := disk.
 
-  Inductive step : Semantics Op State :=
+  Inductive step : forall `(op: Op T), Semantics State T :=
   | step_read : forall a r (state: State),
       (forall b, state a = Some b -> r = b) ->
       step (Read a) state r state
@@ -25,7 +23,6 @@ Module D.
   | step_disk_size : forall (state: State),
       step (DiskSize) state (size state) state.
 
-  Definition exec := Prog.exec step.
-  Definition exec_recover := Prog.exec_recover step.
+  Definition API := {| op_sem := @step |}.
 
 End D.
