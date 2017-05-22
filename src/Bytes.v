e6b4d19f1f2c883a4bc726fd8a6c61e37c1b600d
Tej Chajed <tchajed@mit.edu>
2017-05-22 12:52:17 -0400

    Implement multiple block read/write in Coq
    Not connected to extraction, but dependently typed since [bytes n] keeps
    track of its length.

--- src/Bytes.v
+++ src/Bytes.v
@@ -10,3 +10,9 @@ Parameter bytes : nat -> Type.
 Parameter bytes_dec : forall n, EqDec (bytes n) eq.
 
 Axiom bytes0 : forall n, bytes n.
+
+Axiom bconcat : forall n1 n2, bytes n1 -> bytes n2 -> bytes (n1+n2).
+
+Axiom bsplit : forall n1 n2, bytes (n1+n2) -> bytes n1 * bytes n2.
+
+Arguments bsplit {n1 n2} bs.
---
+++ src/SeqDisk/ArrayAPI.v
@@ -0,0 +1,30 @@
+Require Import Bytes.
+Require Import Disk.
+
+Require Import SeqDisk.SeqDiskAPI.
+Require Import Refinement.ProgLang.
+Require Import Refinement.Interface.
+
+Opaque blockbytes.
+
+Section ArrayDisk.
+
+  Variable (d:Interface D.API).
+
+  Fixpoint read (off:nat) n : prog (bytes (n*blockbytes)) :=
+    match n with
+    | 0 => Ret (bytes0 0)
+    | S n => b <- Prim d (D.Read off);
+              rest <- read (off+1) n;
+              Ret (bconcat b rest)
+    end.
+
+  Fixpoint write (off:nat) n (bs:bytes (n*blockbytes)) {struct n} : prog unit.
+    destruct n; simpl in *.
+    - apply (Ret tt).
+    - destruct (bsplit bs) as [b rest].
+      apply (Bind (Prim d (D.Write off b))); intros _.
+      apply (write (off+1) _ rest).
+  Defined.
+
+End ArrayDisk.
