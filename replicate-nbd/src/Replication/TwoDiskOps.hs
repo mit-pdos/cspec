42785619c4ba8b4d520c30979e661aa8d1f694e4
Tej Chajed <tchajed@mit.edu>
2017-05-24 09:21:08 -0400

    Support initializing with disks missing
    Cleans up the environment handling code so that the state tracked for
    each disk is opaque. Instead, there are just methods to get a Maybe Fd,
    encapsulating failing early if the file never existed and checking if
    the file exists before returning the persistent handle.
    
    Handling the exception is a bit unwieldy, but it was the best way I
    could see from the Control.Exception API to filter to a specific type of
    exception.

--- replicate-nbd/src/Replication/TwoDiskOps.hs
+++ replicate-nbd/src/Replication/TwoDiskOps.hs
@@ -6,26 +6,22 @@ import                   Control.Monad.Reader (reader, liftIO)
 import qualified         Data.ByteString as BS
 import                   Disk
 import                   Replication.TwoDiskEnvironment
-import                   System.Directory (doesFileExist)
 import                   System.IO (SeekMode(..))
-import                   System.Posix.IO (closeFd)
 import "unix-bytestring" System.Posix.IO.ByteString
 import                   System.Posix.Types (Fd)
 import                   TwoDiskAPI
 import                   Utils.Conversion
 
-getDisk :: Coq_diskId -> Env -> (FilePath, Fd)
-getDisk Coq_d0 = disk0
-getDisk Coq_d1 = disk1
+getDisk :: Coq_diskId -> TwoDiskProg (Maybe Fd)
+getDisk Coq_d0 = reader disk0 >>= liftIO
+getDisk Coq_d1 = reader disk1 >>= liftIO
 
 ifExists :: Coq_diskId -> (Fd -> IO a) -> TwoDiskProg (DiskResult a)
 ifExists d m = do
-  (path, fd) <- reader $ getDisk d
-  liftIO $ do
-    exists <- doesFileExist path
-    if exists
-      then Working <$> m fd
-      else closeFd fd >> return Failed
+  mfd <- getDisk d
+  liftIO $ case mfd of
+      Just fd -> Working <$> m fd
+      Nothing -> return Failed
 
 read :: Coq_diskId -> Coq_addr -> TwoDiskProg (DiskResult BS.ByteString)
 read d a = ifExists d $ \fd ->
