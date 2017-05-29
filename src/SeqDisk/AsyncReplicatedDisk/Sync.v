626236155f1e33689e08d63860bf93be2e5dfb61
Tej Chajed <tchajed@mit.edu>
2017-05-29 17:34:13 -0400

    Prove async Sync

---
+++ src/SeqDisk/AsyncReplicatedDisk/Sync.v
@@ -0,0 +1,81 @@
+Require Import Automation.
+Require Import Disk.AsyncDisk.
+
+Require Import Refinement.Interface.
+Require Import Refinement.ProgLang.Prog.
+Require Import Refinement.ProgLang.Hoare.
+Require Import
+        TwoDisk.AsyncTwoDiskAPI
+        TwoDisk.AsyncTwoDiskTheorems
+        TwoDisk.TwoDiskFacts.
+Require Import SeqDisk.AsyncReplicatedDisk.Step.
+
+Require Import MaybeHolds.
+
+Section ReplicatedDisk.
+
+  Variable (td:Interface TD.API).
+
+  Definition Sync : prog unit :=
+    _ <- Prim td (TD.Sync d0);
+      _ <- Prim td (TD.Sync d1);
+      Ret tt.
+
+  Theorem disk0_wipe_flush_stable : forall state d,
+      TD.disk0 state |= then_flush (covered d) ->
+      TD.disk0 (TD.wipe state) |= then_flush (covered d).
+  Proof.
+    intros.
+    destruct state; simpl in *.
+    destruct disk0, disk1; simpl in *; eauto.
+  Qed.
+
+  Theorem disk1_wipe_flush_stable : forall state d,
+      TD.disk1 state |= then_flush (covered d) ->
+      TD.disk1 (TD.wipe state) |= then_flush (covered d).
+  Proof.
+    intros.
+    destruct state; simpl in *.
+    destruct disk0, disk1; simpl in *; eauto.
+  Qed.
+
+  Hint Resolve disk0_wipe_flush_stable disk1_wipe_flush_stable.
+
+  Hint Resolve pred_weaken.
+  Hint Resolve then_wipe_crashesTo.
+
+  Theorem Sync_ok :
+    prog_spec
+      (fun '(d_0, d_1) state =>
+         {|
+           pre :=
+             TD.disk0 state |= covered d_0 /\
+             TD.disk1 state |= covered d_1;
+           post :=
+             fun r state' =>
+               r = tt /\
+               TD.disk0 state' |= then_flush (covered d_0) /\
+               TD.disk1 state' |= then_flush (covered d_1);
+           recover :=
+             fun _ state' =>
+               (TD.disk0 state' |= crashesTo d_0 /\
+                TD.disk1 state' |= crashesTo d_1) \/
+               (TD.disk0 state' |= then_flush (covered d_0) /\
+                TD.disk1 state' |= crashesTo d_1) \/
+               (TD.disk0 state' |= then_flush (covered d_0) /\
+                TD.disk1 state' |= then_flush (covered d_1));
+         |})
+      (Sync)
+      (irec td)
+      (refinement td).
+  Proof.
+    unfold Sync.
+
+    step.
+    step.
+    step.
+  Qed.
+
+End ReplicatedDisk.
+
+Hint Resolve Sync_ok.
