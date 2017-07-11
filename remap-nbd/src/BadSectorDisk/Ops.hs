5863608acee7f1f3c11312f31e730846ca5f8b03
Nickolai Zeldovich <nickolai@csail.mit.edu>
2017-07-11 15:11:58 -0400

    actually return garbage when reading the bad sector

--- remap-nbd/src/BadSectorDisk/Ops.hs
+++ remap-nbd/src/BadSectorDisk/Ops.hs
@@ -4,6 +4,7 @@ module BadSectorDisk.Ops where
 import                   Control.Monad (void)
 import                   Control.Monad.Reader (reader, liftIO)
 import qualified         Data.ByteString as BS
+import qualified         Data.Char
 import                   BadSectorDisk.Env
 import                   Sectors
 import                   System.IO (SeekMode(..))
@@ -15,7 +16,11 @@ import                   Utils.Conversion
 read :: Coq_addr -> TheProg BS.ByteString
 read a = do
   fd <- reader diskHandle
-  liftIO $ fdPread fd blocksize (fromIntegral $ addrToOffset a)
+  bs <- reader badSector
+  if a == bs then
+    return $ BS.replicate blocksize $ fromIntegral $ Data.Char.ord 'A'
+  else
+    liftIO $ fdPread fd blocksize (fromIntegral $ addrToOffset a)
 
 write :: Coq_addr -> BS.ByteString -> TheProg ()
 write a b = do
