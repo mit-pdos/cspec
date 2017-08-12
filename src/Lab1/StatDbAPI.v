fae9d087dc4e94e413c37ef802bbe5675e098885
Frans Kaashoek <kaashoek@mit.edu>
2017-08-12 17:05:08 -0400

    A bit of text, not much

--- src/Lab1/StatDbAPI.v
+++ src/Lab1/StatDbAPI.v
@@ -1,10 +1,52 @@
 Require Import POCS.
 
+(** * Specification for StatDB
+
+    An implementation of StatDB will have to prove that its implementation meets
+    the specification stated in this file.  The specification for StatDB
+    contains three parts:
+
+    - The state of the StatDB: a list of integers
+
+    - An initial state: the list of integers is nil
+
+    - Two operations: add and mean. The specification for each operation is
+    stated in terms of a precondition and a postcondition.  (Each operation also
+    has a recovered condition, but we will ignore crash conditions in this lab.)
+
+*)
+
+
+
+(** ** The state of StatDB:  
+ *)
 
 Definition State := list nat.
 
+(** ** The initial state of StatDB:  
+ *)
+
 Definition inited (s : State) : Prop :=
   s = nil.
+
+(** ** The specification of the StatDB operations
+
+  The specifications are of type [Specification], which is defined in
+  [POCS.Refinement.Hoare]. A specificatoin is a record with 3 fields: [pre],
+  [post], and [recovered] (which we will ignore in this lab).  For both
+  operations the precondition is just [True] (i.e., the operations can be always
+  invoked).  The postconditions describe the effects of the operations [add] and
+  [mean]:
+
+  - [add_spec]'s post condition states that [add v] adds [v] to the state of
+    StatDB and that [add] returns [tt].
+
+  - [mean_spec]'s post condition states that [mean] doesn't modify the state of
+    StatDB, and that its return value is one of two values: (1) if state is
+    [nil], then the return value is [None]; (2) if state is not [nil], then the
+    return value is the average of the numbers in StatDB's state.
+
+ *)
 
 Definition add_spec v : Specification unit unit unit State :=
   fun (_ : unit) state => {|
