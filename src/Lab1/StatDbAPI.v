387e8b1045ac97ba5172bb81fd0cd7fc157d75cb
Frans Kaashoek <kaashoek@mit.edu>
2017-08-14 13:38:23 -0400

    a bit more writing

--- src/Lab1/StatDbAPI.v
+++ src/Lab1/StatDbAPI.v
@@ -18,12 +18,14 @@ Require Import POCS.
 
 
 
-(** ** The state of StatDB:  
+(** ** The state of StatDB
+   
+  We will often refer to the state that specification maintains as the spec state.
  *)
 
 Definition State := list nat.
 
-(** ** The initial state of StatDB:  
+(** ** The initial value of the spec state:
  *)
 
 Definition inited (s : State) : Prop :=
@@ -38,13 +40,13 @@ Definition inited (s : State) : Prop :=
   invoked).  The postconditions describe the effects of the operations [add] and
   [mean]:
 
-  - [add_spec]'s post condition states that [add v] adds [v] to the state of
-    StatDB and that [add] returns [tt].
+  - [add_spec]'s post condition states that [add v] adds [v] to the spec state
+    of StatDB and that [add] returns [tt].
 
-  - [mean_spec]'s post condition states that [mean] doesn't modify the state of
-    StatDB, and that its return value is one of two values: (1) if state is
-    [nil], then the return value is [None]; (2) if state is not [nil], then the
-    return value is the average of the numbers in StatDB's state.
+  - [mean_spec]'s post condition states that [mean] doesn't modify the spec
+    state of StatDB, and that its return value is one of two values: (1) if
+    state is [nil], then the return value is [None]; (2) if state is not [nil],
+    then the return value is the average of the numbers in StatDB's state.
 
  *)
 
@@ -67,20 +69,39 @@ Definition mean_spec : Specification unit (option nat) unit State :=
   |}.
 
 
+(** * StatDB module
+
+   An implementation (the code) of StatDB must implement the StatDB module (the
+   spec).  The module must provide implementation for [init], [add], [mean], and
+   [recovery] (which will be a NOOP).  In addition, the module must provide
+   proofs showing, for example, that its [add] implementation meets the
+   [add_spec].  The proof approach POCS uses is based on backwards simulation
+   and this approach requires the implementation to define [abstr], which
+   abstracts the code state to the spec state (the list of integers that StatDB
+   maintains).  [abstr] is of type [Refinement.Hoare.Abstraction].
+
+*)
+
+
 Module Type StatDbAPI.
 
+  (* The implementation must provide the following methods: *)
   Axiom init : proc InitResult.
   Axiom add : nat -> proc unit.
   Axiom mean : proc (option nat).
   Axiom recover : proc unit.
 
+  (* The abstraction relation between spec and code state *)
   Axiom abstr : Abstraction State.
 
+  (* The proofs that the implementation methods meet their specs: *)
   Axiom init_ok : init_abstraction init recover abstr inited.
   Axiom add_ok : forall v, proc_spec (add_spec v) (add v) recover abstr.
   Axiom mean_ok : proc_spec mean_spec mean recover abstr.
   Axiom recover_noop : rec_noop recover abstr no_crash.
 
+  (* Hints to proof automation to apply the following proofs. The implementation
+  doesn't have to be concerned with these hints. *)
   Hint Resolve init_ok.
   Hint Resolve add_ok.
   Hint Resolve mean_ok.
