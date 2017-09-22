250852ef880baeb9fe8e88cd0dcfefee480ac777
Nickolai Zeldovich <nickolai@csail.mit.edu>
2017-09-22 11:27:43 -0400

    add a newuser mail server function

--- src/FS/MailServerAPI.v
+++ src/FS/MailServerAPI.v
@@ -53,6 +53,17 @@ Definition delete_spec user idx : Specification _ _ unit State :=
     recovered := fun _ _ => False
   |}.
 
+Definition newuser_spec : Specification _ _ unit State :=
+  fun F state => {|
+    pre :=
+      state |= F;
+    post := fun r state' =>
+      exists uid,
+      r = uid /\
+      state' |= F * uid |-> nil;
+    recovered := fun _ _ => False
+  |}.
+
 
 Module Type MailServerAPI.
 
@@ -60,6 +71,7 @@ Module Type MailServerAPI.
   Axiom deliver : user -> message -> proc unit.
   Axiom read : user -> proc mailbox.
   Axiom delete : user -> nat -> proc unit.
+  Axiom newuser : proc user.
   Axiom recover : proc unit.
 
   Axiom abstr : Abstraction State.
@@ -68,12 +80,14 @@ Module Type MailServerAPI.
   Axiom deliver_ok : forall uid msg, proc_spec (deliver_spec uid msg) (deliver uid msg) recover abstr.
   Axiom read_ok : forall uid, proc_spec (read_spec uid) (read uid) recover abstr.
   Axiom delete_ok : forall uid i, proc_spec (delete_spec uid i) (delete uid i) recover abstr.
+  Axiom newuser_ok : proc_spec newuser_spec newuser recover abstr.
   Axiom recover_noop : rec_noop recover abstr no_crash.
 
   Hint Resolve init_ok.
   Hint Resolve deliver_ok.
   Hint Resolve read_ok.
   Hint Resolve delete_ok.
+  Hint Resolve newuser_ok.
   Hint Resolve recover_noop.
 
 End MailServerAPI.
