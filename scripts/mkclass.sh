74faa4c8f4541cebd67630030d8d26bc026a2972
Nickolai Zeldovich <nickolai@csail.mit.edu>
2017-10-12 17:46:05 -0400

    some initial boilerplate for exporting lab 3

--- scripts/mkclass.sh
+++ scripts/mkclass.sh
@@ -11,13 +11,13 @@ umask 2
 CLASSREPO=git@github.com:mit-pdos/6.826-2017-labs
 
 # include only these src/ directories 
-SRCS="src/Helpers src/Spec src/Lab1 src/Common src/Lab2"
+SRCS="src/Helpers src/Spec src/Common src/Lab1 src/Lab2 src/Lab3"
 
 # files/directories to copy
 TOPLEVELS="README.md LICENSE _CoqProject .gitignore scripts/add-preprocess.sh src/POCS.v statdb-cli remap-nbd"
 
 # current Makefile
-MAKEFILE_NAME=Makefile.lab2
+MAKEFILE_NAME=Makefile.lab3
 
 SD=$(cd $(dirname $0)/.. && /bin/pwd)
 CD=/tmp/pocs.$$
