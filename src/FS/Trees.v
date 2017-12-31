8c9957ed0d1e0d9f881aab0fa0d638155cf6f65e
Frans Kaashoek <kaashoek@mit.edu>
2017-12-31 11:08:05 -0500

    Checkpoint switching Graph to MSetWeakLists. Need a few Ltacs, but seems to work out.

--- src/FS/Trees.v
+++ src/FS/Trees.v
@@ -1,5 +1,7 @@
 Require Import POCS.
 Require Import String.
+Require Import Equalities.
+Require Import MSets.MSetWeakList.
 
 Import ListNotations.
 Open Scope string.
@@ -18,30 +20,61 @@ Record Link := mkLink {
   LinkName : string
 }.
 
-Definition Graph := list Link.
+Instance Node_equal_dec : EqualDec Node.
+  unfold EqualDec; intros.
+  destruct x, y; try (right; congruence).
+  destruct (eq_nat_dec dirnum dirnum0); subst; eauto.
+    right; congruence.
+  destruct (eq_nat_dec filenum filenum0); subst; eauto.
+    right; congruence.
+  destruct (list_eq_dec string_dec target target0); subst; eauto.
+    right; congruence.
+Defined.
+
+Lemma Link_equal_dec : EqualDec Link.
+  unfold EqualDec; intros.
+  destruct x, y.
+  destruct (eq_nat_dec LinkFrom0 LinkFrom1); subst.
+  destruct (Node_equal_dec LinkTo0 LinkTo1); subst.
+  destruct (string_dec LinkName0 LinkName1); subst.
+  eauto.
+  right; congruence.
+  right; congruence.
+  right; congruence.
+Defined.
+
+Module MDT_Link.
+  Definition t := Link.
+  Definition eq_dec := Link_equal_dec.
+End MDT_Link.
+
+Module Link_as_UDT := Make_UDT(MDT_Link).
+
+Module Graph := MSetWeakList.Make Link_as_UDT.
+
+Definition t := Graph.singleton (mkLink 0 (FileNode 0) "x").
 
 Definition File := string.
 Definition Files := list File.
 
 Record FS := mkFS {
   FSRoot : nat;     (* root DirNode *)
-  FSLinks : Graph;
+  FSLinks : Graph.t;
   FSFiles : Files;  (* [filenum]s point into this list *)
 }.
 
 Definition Pathname := list string.
 
-
 (** [path_evaluates] is used to specify lookup *)
 
 Inductive valid_link : forall (fs : FS) (dir : nat) (name : string) (target : Node), Prop :=
 | ValidLink : forall fs dir name target,
-  In (mkLink dir target name) (FSLinks fs) ->
+  Graph.In (mkLink dir target name) (FSLinks fs) ->
   valid_link fs dir name target
 | ValidDot : forall fs dir,
   valid_link fs dir "." (DirNode dir)
 | ValidDotDot : forall fs dir name parent,
-  In (mkLink parent (DirNode dir) name) (FSLinks fs) ->
+  Graph.In (mkLink parent (DirNode dir) name) (FSLinks fs) ->
   valid_link fs dir ".." (DirNode parent)
 | ValidDotDotRoot : forall fs dir,
   dir = FSRoot fs ->
@@ -55,7 +88,7 @@ Inductive path_evaluates : forall (fs : FS) (start : Node) (pn : Pathname) (targ
   path_evaluates fs namenode pn target ->
   path_evaluates fs (DirNode startdir) (name :: pn) target
 | PathEvalSymlink : forall fs startdir name sympath symtarget pn target,
-  In (mkLink startdir (SymlinkNode sympath) name) (FSLinks fs) ->
+  Graph.In (mkLink startdir (SymlinkNode sympath) name) (FSLinks fs) ->
   path_evaluates fs (DirNode startdir) sympath symtarget ->
   path_evaluates fs symtarget pn target ->
   path_evaluates fs (DirNode startdir) (name :: pn) target.
@@ -68,56 +101,26 @@ Definition path_eval_root (fs : FS) (pn : Pathname) (target : Node) : Prop :=
 
 Hint Constructors valid_link.
 Hint Constructors path_evaluates.
-Hint Resolve in_eq.
-Hint Resolve in_cons.
-Hint Extern 1 (In _ _) => compute.
-
-Instance Node_equal_dec : EqualDec Node.
-  unfold EqualDec; intros.
-  destruct x, y; try (right; congruence).
-  destruct (eq_nat_dec dirnum dirnum0); subst; eauto.
-    right; congruence.
-  destruct (eq_nat_dec filenum filenum0); subst; eauto.
-    right; congruence.
-  destruct (list_eq_dec string_dec target target0); subst; eauto.
-    right; congruence.
-Defined.
-
-Instance Link_equal_dec : EqualDec Link.
-  unfold EqualDec; intros.
-  destruct x, y.
-  destruct (eq_nat_dec LinkFrom0 LinkFrom1); subst.
-  destruct (Node_equal_dec LinkTo0 LinkTo1); subst.
-  destruct (string_dec LinkName0 LinkName1); subst.
-  eauto.
-  right; congruence.
-  right; congruence.
-  right; congruence.
-Defined.
 
 Hint Extern 1 False =>
   match goal with
   | H : {| LinkFrom := ?a; LinkTo := ?b; LinkName := ?c |} =
         {| LinkFrom := ?d; LinkTo := ?e; LinkName := ?f |} |- _ =>
-    destruct (Link_equal_dec (mkLink a b c) (mkLink d e f)); congruence
+    destruct (Graph.eq_dec (mkLink a b c) (mkLink d e f)); congruence
   end.
-
 
 (** Definition of concurrent tree modifications, to write specifications *)
 
-Definition tree_transform := Graph -> Graph.
+Definition tree_transform := Graph.t -> Graph.t.
 
 Definition transform_fs (fs : FS) (xform : tree_transform) :=
   mkFS (FSRoot fs) (xform (FSLinks fs)) (FSFiles fs).
 
 Definition add_link (srcdir : nat) (dst : Node) (name : string) : tree_transform :=
-  fun links => mkLink srcdir dst name :: links.
+  fun links => Graph.add (mkLink srcdir dst name) links.
 
 Definition remove_link (srcdir : nat) (dst : Node) (name : string) : tree_transform :=
-  fun links => filter (fun l => match Link_equal_dec l (mkLink srcdir dst name) with
-    | left _ => false
-    | right _ => true
-    end) links.
+  fun links => Graph.remove (mkLink srcdir dst name) links.
 
 Definition xform_both (x1 x2 : tree_transform) :=
   fun t => x2 (x1 t).
@@ -126,7 +129,6 @@ Definition xform_id : tree_transform :=
   fun t => t.
 
 Notation "x1 ;; x2" := (xform_both x1 x2) (at level 50).
-
 
 (** This is what a specification looks like *)
 
@@ -152,8 +154,6 @@ Definition lookup_spec (pn : Pathname) : specification (option Node) := {|
   Result := fun result fs =>
     (exists node, result = Some node /\ path_eval_root fs pn node) \/
     result = None /\ ~ exists node, path_eval_root fs pn node;
-
-
   AddLinks := xform_id;
   RemoveLinks := xform_id;
 |}.
@@ -196,23 +196,43 @@ Definition rename_spec srcdir srcname dstdir dstname := {|
 (** Example valid (and some invalid) lookups *)
 
 Definition example_fs := mkFS 1
-  [ mkLink 1 (DirNode 2) "etc";
-    mkLink 2 (FileNode 10) "passwd";
-    mkLink 2 (SymlinkNode ["passwd"]) "passwd~";
-    mkLink 1 (SymlinkNode ["etc"]) "etc~";
-    mkLink 1 (DirNode 3) "tmp";
-    mkLink 3 (SymlinkNode [".."; "etc"]) "foo";
-    mkLink 3 (SymlinkNode [".."; ".."; "etc"]) "foo2";
-    mkLink 3 (SymlinkNode [".."]) "root"
-  ]
+ (Graph.add (mkLink 1 (DirNode 2) "etc")
+ (Graph.add (mkLink 2 (FileNode 10) "passwd")                            
+ (Graph.add (mkLink 2 (SymlinkNode ["passwd"]) "passwd~")
+ (Graph.add (mkLink 1 (SymlinkNode ["etc"]) "etc~")
+ (Graph.add (mkLink 1 (DirNode 3) "tmp")
+ (Graph.add (mkLink 3 (SymlinkNode [".."; "etc"]) "foo")
+ (Graph.add (mkLink 3 (SymlinkNode [".."; ".."; "etc"]) "foo2")
+ (Graph.add (mkLink 3 (SymlinkNode [".."]) "root")
+             Graph.empty))))))))
   [].
 
 Theorem etc_passwd :
   spec_ok example_fs (lookup_spec ["etc"; "passwd"]) (Some (FileNode 10)).
 Proof.
-  compute.
-  eauto 20.
+  simpl.
+  left.
+  eexists.
+  simpl; subst.
+  split.
+  Focus 2.
+  
+  econstructor; simpl.
+  constructor; simpl.
+  apply Graph.add_spec.
+  eauto.
+
+  econstructor; simpl.
+  constructor; simpl.
+  apply Graph.add_spec.
+  right.
+  apply Graph.add_spec.
+  eauto.
+  eauto. reflexivity. 
 Qed.
+
+
+(*
 
 Theorem etc_passwd' :
   spec_ok example_fs (lookup_spec ["etc"; "passwd~"]) (Some (FileNode 10)).
@@ -228,7 +248,6 @@ Proof.
   eauto 20.
 Qed.
 
-(*
 Theorem tmp_foo_passwd :
   spec_ok example_fs (lookup_spec ["tmp"; "foo"; "passwd"]) (Some (FileNode 10)).
 Proof.
@@ -247,13 +266,36 @@ Qed.
 Theorem no_usr :
   spec_ok example_fs (lookup_spec ["usr"]) None.
 Proof.
-  compute.
-
-  right. intuition. deex.
+  simpl.
+  right; eauto.
+  split; eauto.
+  intuition. deex.
   inversion H; clear H; subst.
   inversion H4; clear H4; subst.
-  compute in *. intuition.
-  compute in *. intuition.
+
+  apply Graph.add_spec in H.
+  intuition idtac; try congruence.
+  apply Graph.add_spec in H0.
+  intuition idtac; try congruence.
+  apply Graph.add_spec in H.
+  intuition idtac; try congruence.
+  apply Graph.add_spec in H0.
+  intuition idtac; try congruence.
+  apply Graph.add_spec in H.
+  intuition idtac; try congruence.
+  apply Graph.add_spec in H0.
+  apply Graph.add_spec in H0.
+  intuition idtac; try congruence.
+  apply Graph.add_spec in H0.
+  intuition idtac; try congruence.
+  apply Graph.add_spec in H.
+  intuition idtac; try congruence.
+  apply Graph.add_spec in H0.
+  intuition idtac; try congruence.
+  apply Graph.is_empty_spec in H; eauto.
+
+  (* again ... *)
+    
 Qed.
 
 
