594a37c2a572e95d7ff9c2a5dcfa57c4fc564b81
Nickolai Zeldovich <nickolai@csail.mit.edu>
2018-03-02 15:26:25 -0500

    implement decode_tid_fn too
    now the mail server really delivers messages into /tmp/mailtest!
    but it quickly runs out of maximum filename length because each
    new message grows the filename by 1:
    
    /tmp/mailtest/mail/0.aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa

--- mail-test/Extract.v
+++ mail-test/Extract.v
@@ -10,6 +10,7 @@ Require Import Mail2.MailServer.
 Require Import Mail2.MailFSStringAbsAPI.
 
 Extract Inlined Constant encode_tid_fn => "Support.encode_tid_fn".
+Extract Inlined Constant decode_tid_fn => "Support.decode_tid_fn".
 
 Separate Extraction ms_bottom.
 
--- mail-test/lib/Support.hs
+++ mail-test/lib/Support.hs
@@ -1,4 +1,13 @@
 module Support where
 
+import Data.List
+
 encode_tid_fn :: Integer -> String -> String
 encode_tid_fn tid fn = (show tid) ++ "." ++ fn
+
+decode_tid_fn :: String -> (Integer, String)
+decode_tid_fn fn =
+  case findIndex (=='.') fn of
+    Nothing -> (0, fn)
+    Just i -> let (tidstr, fnstr) = splitAt i fn in
+      (read tidstr, tail fnstr)
