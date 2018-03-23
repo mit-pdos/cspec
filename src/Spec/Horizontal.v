7da2abb9fb9cc73407c2caed5ffee3f423d08b1c
Nickolai Zeldovich <nickolai@csail.mit.edu>
2018-03-23 09:43:28 -0400

    prove left- and right-movers are preserved by horizontal composition

--- src/Spec/Horizontal.v
+++ src/Spec/Horizontal.v
@@ -8,6 +8,7 @@ Require Import List.
 Require Import Modules.
 Require Import Ordering.
 Require Import Abstraction.
+Require Import Movers.
 
 Import ListNotations.
 
@@ -52,6 +53,7 @@ End HorizontalComposition.
 
 Arguments horizState indexT {cmp} sliceState.
 Arguments horizStep indexT {cmp sliceOpT sliceState} sliceStep.
+Arguments Slice {indexT sliceOpT} i {T}.
 
 
 Section HorizontalCompositionAbs.
@@ -129,3 +131,125 @@ Section HorizontalCompositionAbs.
 End HorizontalCompositionAbs.
 
 Arguments horizAbsR indexT {cmp sliceState1 sliceState2} absR.
+
+
+Section HorizontalCompositionMovers.
+
+  Variable indexT : Type.
+  Context {cmp : Ordering indexT}.
+
+  Variable sliceOpT : Type -> Type.
+  Variable sliceState : Type.
+  Variable sliceStep : OpSemantics sliceOpT sliceState.
+
+  Hint Resolve FMap.add_mapsto.
+  Hint Resolve FMap.mapsto_add_ne'.
+
+  Theorem horiz_right_mover_ok :
+    forall `(op : sliceOpT T),
+      right_mover sliceStep op ->
+      forall i,
+        right_mover (horizStep indexT sliceStep) (Slice i op).
+  Proof.
+    intros.
+    unfold right_mover; intros.
+    inversion H0; clear H0; subst; repeat sigT_eq.
+    eapply H in H10 as H'; intuition subst.
+    inversion H3; clear H3; subst; repeat sigT_eq.
+    destruct (i == idx); subst.
+    - eapply FMap.mapsto_add in H6; subst.
+      eapply H1 in H11; eauto; deex.
+      eexists; split.
+      + econstructor; eauto.
+      + rewrite FMap.add_add.
+        erewrite <- FMap.add_add with (v1 := s'0).
+        econstructor; eauto.
+    - eapply FMap.mapsto_add_ne in H6; eauto.
+      eexists; split.
+      + econstructor; eauto.
+      + rewrite FMap.add_add_ne by eauto.
+        econstructor; eauto.
+  Qed.
+
+  Theorem horiz_enabled_stable :
+    forall `(op : sliceOpT T),
+      enabled_stable sliceStep T op ->
+      forall i,
+        enabled_stable (horizStep indexT sliceStep) T (Slice i op).
+  Proof.
+    intros.
+    unfold enabled_stable; intros.
+    unfold enabled_in in *; repeat deex.
+    inversion H1; clear H1; subst; repeat sigT_eq.
+    inversion H2; clear H2; subst; repeat sigT_eq.
+    destruct (i == idx); subst.
+    - replace s0 with s1 in * by ( eapply FMap.mapsto_unique; eauto ).
+      edestruct H.
+      2: unfold enabled_in; eauto.
+      2: eauto.
+      eauto.
+      repeat deex.
+      do 3 eexists. econstructor; eauto.
+    - do 3 eexists. econstructor; eauto.
+  Qed.
+
+  Theorem horiz_left_mover_ok :
+    forall `(op : sliceOpT T),
+      left_mover sliceStep op ->
+      forall i,
+        left_mover (horizStep indexT sliceStep) (Slice i op).
+  Proof.
+    intros.
+    split; intros.
+    - eapply horiz_enabled_stable.
+      destruct H; eauto.
+    - inversion H0; clear H0; subst; repeat sigT_eq.
+      eapply H in H10 as H'; intuition subst.
+      inversion H3; clear H3; subst; repeat sigT_eq.
+      destruct (i == idx); subst.
+      * eapply FMap.mapsto_add in H7; subst.
+        eapply H1 in H11; eauto; deex.
+        eexists; split.
+        + econstructor; eauto.
+        + rewrite FMap.add_add.
+          erewrite <- FMap.add_add with (v1 := s').
+          econstructor; eauto.
+      * eapply FMap.mapsto_add_ne in H7; eauto.
+        eexists; split.
+        + econstructor; eauto.
+        + rewrite FMap.add_add_ne by eauto.
+          econstructor; eauto.
+  Qed.
+
+  Theorem horiz_left_mover_pred_ok :
+    forall `(op : sliceOpT T) P ,
+      left_mover_pred sliceStep op P ->
+      forall i,
+        left_mover_pred (horizStep indexT sliceStep) (Slice i op)
+          (fun tid S => exists s, FMap.MapsTo i s S /\ P tid s).
+  Proof.
+    intros.
+    split; intros.
+    - eapply horiz_enabled_stable.
+      destruct H; eauto.
+    - inversion H0; clear H0; subst; repeat sigT_eq.
+      eapply H in H10 as H'; intuition subst; deex.
+      inversion H4; clear H4; subst; repeat sigT_eq.
+      destruct (i == idx); subst.
+      * replace s2 with s3 in * by ( eapply FMap.mapsto_unique; eauto ).
+        eapply FMap.mapsto_add in H7; subst.
+        eapply H1 in H13; eauto; deex.
+        eexists; split.
+        + econstructor; eauto.
+        + rewrite FMap.add_add.
+          erewrite <- FMap.add_add with (v1 := s').
+          econstructor; eauto.
+      * eapply FMap.mapsto_add_ne in H7; eauto.
+        replace s2 with s in * by ( eapply FMap.mapsto_unique; eauto ).
+        eexists; split.
+        + econstructor; eauto.
+        + rewrite FMap.add_add_ne by eauto.
+          econstructor; eauto.
+  Qed.
+
+End HorizontalCompositionMovers.
