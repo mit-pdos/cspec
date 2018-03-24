dd364b252b2c3db928478946190668b53ffe2f2d
Nickolai Zeldovich <nickolai@csail.mit.edu>
2018-03-24 08:58:43 -0400

    conver TryDeliverImpl.v to new module style

--- src/Mail2/TryDeliverImpl.v
+++ src/Mail2/TryDeliverImpl.v
@@ -6,10 +6,11 @@ Require Import MailFSAPI.
 Require Import MailboxTmpAbsAPI.
 
 
-Module TryDeliverImpl <:
-  LayerImpl
-    MailFSOp MailboxTmpAbsState MailFSAPI
-    TryDeliverOp MailboxTmpAbsState TryDeliverAPI.
+Module TryDeliverImpl' <:
+  LayerImplMoversT
+    MailboxTmpAbsState
+    MailFSOp     MailFSAPI
+    TryDeliverOp TryDeliverAPI.
 
   Definition linkmail_core :=
     ts <- Op MailFSOp.Random;
@@ -28,6 +29,13 @@ Module TryDeliverImpl <:
     | TryDeliverOp.Unlock => Op (MailFSOp.Unlock)
     | TryDeliverOp.Ext extop => Op (MailFSOp.Ext extop)
     end.
+
+  Theorem compile_op_no_atomics :
+    forall `(op : _ T),
+      no_atomics (compile_op op).
+  Proof.
+    destruct op; compute; eauto.
+  Qed.
 
   Ltac step_inv :=
     match goal with
@@ -53,19 +61,22 @@ Module TryDeliverImpl <:
 
   Hint Resolve random_right_mover.
 
-  Theorem linkmail_atomic : forall `(rx : _ -> proc _ T),
-    trace_incl MailFSAPI.step
-      (Bind (compile_op (TryDeliverOp.LinkMail)) rx)
-      (Bind (atomize compile_op (TryDeliverOp.LinkMail)) rx).
+  Theorem ysa_movers_linkmail_core :
+    ysa_movers MailFSAPI.step linkmail_core.
   Proof.
-    intros.
-    eapply trace_incl_atomize_ysa.
-    simpl.
-    unfold linkmail_core, ysa_movers.
-    eauto 20.
+    econstructor; eauto 20.
   Qed.
 
-  Theorem my_compile_correct :
+  Hint Resolve ysa_movers_linkmail_core.
+
+  Theorem ysa_movers : forall `(op : _ T),
+    ysa_movers MailFSAPI.step (compile_op op).
+  Proof.
+    destruct op; simpl; eauto 20.
+    all: admit.
+  Admitted.
+
+  Theorem compile_correct :
     compile_correct compile_op MailFSAPI.step TryDeliverAPI.step.
   Proof.
     unfold compile_correct; intros.
@@ -74,63 +85,12 @@ Module TryDeliverImpl <:
     all: repeat atomic_exec_inv; repeat step_inv; eauto.
   Qed.
 
-  Theorem my_atomize_correct :
-    atomize_correct compile_op MailFSAPI.step.
-  Proof.
-    unfold atomize_correct; intros.
-    destruct op; try trace_incl_simple.
+End TryDeliverImpl'.
 
-    rewrite linkmail_atomic.
-    eapply trace_incl_bind_a; eauto.
-  Qed.
 
-  Hint Resolve my_compile_correct.
-  Hint Resolve my_atomize_correct.
-
-  Theorem all_traces_match :
-    forall ts1 ts2,
-      proc_match (Compile.compile_ok compile_op) ts1 ts2 ->
-      traces_match_ts MailFSAPI.step TryDeliverAPI.step ts1 ts2.
-  Proof.
-    intros.
-    eapply Compile.compile_traces_match_ts; eauto.
-  Qed.
-
-  Definition absR (s1 : MailboxTmpAbsState.State) (s2 : MailboxTmpAbsState.State) :=
-    s1 = s2.
-
-  Definition compile_ts := Compile.compile_ts compile_op.
-
-  Theorem compile_ts_no_atomics :
-    forall ts,
-      no_atomics_ts ts ->
-      no_atomics_ts (compile_ts ts).
-  Proof.
-    eapply Compile.compile_ts_no_atomics.
-    destruct op; compute; eauto.
-  Qed.
-
-  Theorem compile_traces_match :
-    forall ts2,
-      no_atomics_ts ts2 ->
-      traces_match_abs absR
-        MailboxTmpAbsState.initP
-        MailFSAPI.step
-        TryDeliverAPI.step (compile_ts ts2) ts2.
-  Proof.
-    unfold traces_match_abs; intros.
-    rewrite H2 in *; clear H2.
-    eapply all_traces_match; eauto.
-    eapply Compile.compile_ts_ok; eauto.
-  Qed.
-
-  Theorem absInitP :
-    forall s1 s2,
-      MailboxTmpAbsState.initP s1 ->
-      absR s1 s2 ->
-      MailboxTmpAbsState.initP s2.
-  Proof.
-    eauto.
-  Qed.
-
-End TryDeliverImpl.
+Module TryDeliverImpl :=
+  LayerImplMovers
+    MailboxTmpAbsState
+    MailFSOp     MailFSAPI
+    TryDeliverOp TryDeliverAPI
+    TryDeliverImpl'.
