e1b4eb96f6d755384599c37dec02ef5fb745f4c7
Frans Kaashoek <kaashoek@mit.edu>
2018-03-25 16:56:51 -0400

    Convert MailFSPathImpl to new module plan.

--- src/Mail2/MailFSPathImpl.v
+++ src/Mail2/MailFSPathImpl.v
@@ -6,10 +6,11 @@ Require Import MailFSPathAbsAPI.
 Require Import MailFSPathAPI.
 
 
-Module MailFSPathImpl <:
-  LayerImpl
-    MailFSPathOp MailFSPathAbsState MailFSPathAPI
-    MailFSStringOp MailFSPathAbsState MailFSPathAbsAPI.
+Module MailFSPathImpl' <:
+  LayerImplMoversT
+    MailFSPathAbsState 
+    MailFSPathOp MailFSPathAPI
+    MailFSStringOp MailFSPathAbsAPI.
 
   Definition compile_op T (op : MailFSStringOp.opT T) : proc _ T :=
     match op with
@@ -26,6 +27,13 @@ Module MailFSPathImpl <:
     | MailFSStringOp.Random => Op (MailFSPathOp.Random)
     end.
 
+  Theorem compile_op_no_atomics :
+    forall `(op : _ T),
+      no_atomics (compile_op op).
+  Proof.
+    destruct op; compute; eauto.
+  Qed.
+
   Ltac step_inv :=
     match goal with
     | H : MailFSPathAbsAPI.step _ _ _ _ _ _ |- _ =>
@@ -37,7 +45,7 @@ Module MailFSPathImpl <:
   Hint Extern 1 (MailFSPathAbsAPI.step _ _ _ _ _ _) => econstructor.
   Hint Extern 1 (MailFSPathAPI.step _ _ _ _ _ _) => econstructor.
 
-  Theorem my_compile_correct :
+  Theorem compile_correct :
     compile_correct compile_op MailFSPathAPI.step MailFSPathAbsAPI.step.
   Proof.
     unfold compile_correct; intros.
@@ -46,60 +54,18 @@ Module MailFSPathImpl <:
     all: try solve [ repeat atomic_exec_inv; repeat step_inv; eauto ].
   Qed.
 
-  Theorem my_atomize_correct :
-    atomize_correct compile_op MailFSPathAPI.step.
+  Theorem ysa_movers : forall `(op : _ T),
+    ysa_movers MailFSPathAPI.step (compile_op op).
   Proof.
-    unfold atomize_correct; intros.
-    destruct op; trace_incl_simple.
-  Qed.
+    destruct op; simpl; eauto 20.
+    all: admit.
+  Admitted.
 
-  Hint Resolve my_compile_correct.
-  Hint Resolve my_atomize_correct.
+End MailFSPathImpl'.
 
-  Theorem all_traces_match :
-    forall ts1 ts2,
-      proc_match (Compile.compile_ok compile_op) ts1 ts2 ->
-      traces_match_ts MailFSPathAPI.step MailFSPathAbsAPI.step ts1 ts2.
-  Proof.
-    intros.
-    eapply Compile.compile_traces_match_ts; eauto.
-  Qed.
 
-  Definition absR (s1 : MailFSPathAbsState.State) (s2 : MailFSPathAbsState.State) :=
-    s1 = s2.
-
-  Definition compile_ts := Compile.compile_ts compile_op.
-
-  Theorem compile_ts_no_atomics :
-    forall ts,
-      no_atomics_ts ts ->
-      no_atomics_ts (compile_ts ts).
-  Proof.
-    eapply Compile.compile_ts_no_atomics.
-    destruct op; compute; eauto.
-  Qed.
-
-  Theorem compile_traces_match :
-    forall ts2,
-      no_atomics_ts ts2 ->
-      traces_match_abs absR
-        MailFSPathAbsState.initP
-        MailFSPathAPI.step
-        MailFSPathAbsAPI.step (compile_ts ts2) ts2.
-  Proof.
-    unfold traces_match_abs; intros.
-    rewrite H2 in *; clear H2.
-    eapply all_traces_match; eauto.
-    eapply Compile.compile_ts_ok; eauto.
-  Qed.
-
-  Theorem absInitP :
-    forall s1 s2,
-      MailFSPathAbsState.initP s1 ->
-      absR s1 s2 ->
-      MailFSPathAbsState.initP s2.
-  Proof.
-    eauto.
-  Qed.
-
-End MailFSPathImpl.
+Module MailFSPathImpl :=
+  LayerImplMovers
+    MailFSPathAbsState 
+    MailFSPathOp MailFSPathAPI
+    MailFSStringOp MailFSPathAbsAPI.
