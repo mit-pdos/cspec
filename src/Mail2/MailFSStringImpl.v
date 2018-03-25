908d19ed1600358f1352ed6c18ad9852987aadd8
Frans Kaashoek <kaashoek@mit.edu>
2018-03-25 16:35:15 -0400

    Checkpoint converting to new module plan. Ran into (small?) issue with proving ysa_movers_list_core.

--- src/Mail2/MailFSStringImpl.v
+++ src/Mail2/MailFSStringImpl.v
@@ -6,11 +6,12 @@ Require Import MailFSStringAPI.
 Require Import MailFSStringAbsAPI.
 
 
-Module MailFSStringImpl <:
-  LayerImpl
-    MailFSStringOp MailFSStringAbsState MailFSStringAPI
-    MailFSOp MailFSStringAbsState MailFSStringAbsAPI.
-
+Module MailFSStringImpl' <:
+  LayerImplMoversT
+    MailFSStringAbsState
+    MailFSStringOp MailFSStringAPI
+    MailFSOp MailFSStringAbsAPI.
+    
   Definition createwritetmp_core data :=
     tid <- Op (MailFSStringOp.GetTID);
     r <- Op (MailFSStringOp.CreateWriteTmp (encode_tid_fn tid 0) data);
@@ -30,7 +31,7 @@ Module MailFSStringImpl <:
     l <- Op (MailFSStringOp.List);
     Ret (map decode_tid_fn l).
 
-  Definition compile_op T (op : MailFSOp.opT T) : proc _ T :=
+  Definition compile_op T (op : MailFSOp.opT T) : proc MailFSStringOp.opT T :=
     match op with
     | MailFSOp.LinkMail m => linkmail_core m
     | MailFSOp.List => list_core
@@ -45,6 +46,15 @@ Module MailFSStringImpl <:
     | MailFSOp.Random => Op (MailFSStringOp.Random)
     end.
 
+  
+  Theorem compile_op_no_atomics :
+    forall `(op : _ T),
+      no_atomics (compile_op op).
+  Proof.
+    destruct op; compute; eauto.
+  Qed.
+
+  
   Ltac step_inv :=
     match goal with
     | H : MailFSStringAbsAPI.step _ _ _ _ _ _ |- _ =>
@@ -69,55 +79,47 @@ Module MailFSStringImpl <:
 
   Hint Resolve gettid_right_mover.
 
-  Theorem linkmail_atomic : forall `(rx : _ -> proc _ T) m,
-    trace_incl MailFSStringAPI.step
-      (Bind (compile_op (MailFSOp.LinkMail m)) rx)
-      (Bind (atomize compile_op (MailFSOp.LinkMail m)) rx).
+  Theorem ysa_movers_linkmail_core: forall n,
+    ysa_movers MailFSStringAPI.step (linkmail_core n).
   Proof.
-    intros.
-    eapply trace_incl_atomize_ysa.
-    simpl.
-    unfold linkmail_core, ysa_movers.
-    eauto 20.
+    econstructor; eauto 20.
   Qed.
 
-  Theorem list_atomic : forall `(rx : _ -> proc _ T),
-    trace_incl MailFSStringAPI.step
-      (Bind (compile_op (MailFSOp.List)) rx)
-      (Bind (atomize compile_op (MailFSOp.List)) rx).
+  Hint Resolve ysa_movers_linkmail_core.
+
+  Theorem ysa_movers_list_core:
+    ysa_movers MailFSStringAPI.step list_core.
   Proof.
-    intros.
-    eapply trace_incl_atomize_ysa.
-    simpl.
-    unfold list_core, ysa_movers.
-    eauto 20.
+    econstructor; eauto 20.
+    (* XXX Ret too complicated? *)
+  Admitted.
+
+  Hint Resolve ysa_movers_list_core.
+
+  Theorem ysa_movers_createwritetmp_core: forall s,
+    ysa_movers MailFSStringAPI.step (createwritetmp_core s).
+  Proof.
+    econstructor; eauto 20.
   Qed.
 
-  Theorem createwritetmp_atomic : forall `(rx : _ -> proc _ T) fn,
-    trace_incl MailFSStringAPI.step
-      (Bind (compile_op (MailFSOp.CreateWriteTmp fn)) rx)
-      (Bind (atomize compile_op (MailFSOp.CreateWriteTmp fn)) rx).
+  Hint Resolve ysa_movers_createwritetmp_core.
+
+  Theorem ysa_movers_unlinktmp_core:
+    ysa_movers MailFSStringAPI.step unlinktmp_core.
   Proof.
-    intros.
-    eapply trace_incl_atomize_ysa.
-    simpl.
-    unfold createwritetmp_core, ysa_movers.
-    eauto 20.
+    econstructor; eauto 20.
   Qed.
 
-  Theorem unlinktmp_atomic : forall `(rx : _ -> proc _ T),
-    trace_incl MailFSStringAPI.step
-      (Bind (compile_op (MailFSOp.UnlinkTmp)) rx)
-      (Bind (atomize compile_op (MailFSOp.UnlinkTmp)) rx).
-  Proof.
-    intros.
-    eapply trace_incl_atomize_ysa.
-    simpl.
-    unfold unlinktmp_core, ysa_movers.
-    eauto 20.
-  Qed.
+  Hint Resolve ysa_movers_unlinktmp_core.
 
-  Theorem my_compile_correct :
+  Theorem ysa_movers : forall `(op : _ T),
+    ysa_movers MailFSStringAPI.step (compile_op op).
+  Proof.
+    destruct op; simpl; eauto 20.
+    all: admit.
+  Admitted.
+
+  Theorem compile_correct :
     compile_correct compile_op MailFSStringAPI.step MailFSStringAbsAPI.step.
   Proof.
     unfold compile_correct; intros.
@@ -135,69 +137,10 @@ Module MailFSStringImpl <:
     eauto.
   Qed.
 
-  Theorem my_atomize_correct :
-    atomize_correct compile_op MailFSStringAPI.step.
-  Proof.
-    unfold atomize_correct; intros.
-    destruct op; try trace_incl_simple.
+End MailFSStringImpl'.
 
-    + rewrite createwritetmp_atomic.
-      eapply trace_incl_bind_a; eauto.
-    + rewrite linkmail_atomic.
-      eapply trace_incl_bind_a; eauto.
-    + rewrite unlinktmp_atomic.
-      eapply trace_incl_bind_a; eauto.
-    + rewrite list_atomic.
-      eapply trace_incl_bind_a; eauto.
-  Qed.
-
-  Hint Resolve my_compile_correct.
-  Hint Resolve my_atomize_correct.
-
-  Theorem all_traces_match :
-    forall ts1 ts2,
-      proc_match (Compile.compile_ok compile_op) ts1 ts2 ->
-      traces_match_ts MailFSStringAPI.step MailFSStringAbsAPI.step ts1 ts2.
-  Proof.
-    intros.
-    eapply Compile.compile_traces_match_ts; eauto.
-  Qed.
-
-  Definition absR (s1 : MailFSStringAbsState.State) (s2 : MailFSStringAbsState.State) :=
-    s1 = s2.
-
-  Definition compile_ts := Compile.compile_ts compile_op.
-
-  Theorem compile_ts_no_atomics :
-    forall ts,
-      no_atomics_ts ts ->
-      no_atomics_ts (compile_ts ts).
-  Proof.
-    eapply Compile.compile_ts_no_atomics.
-    destruct op; compute; eauto.
-  Qed.
-
-  Theorem compile_traces_match :
-    forall ts2,
-      no_atomics_ts ts2 ->
-      traces_match_abs absR
-        MailFSStringAbsState.initP
-        MailFSStringAPI.step
-        MailFSStringAbsAPI.step (compile_ts ts2) ts2.
-  Proof.
-    unfold traces_match_abs; intros.
-    rewrite H2 in *; clear H2.
-    eapply all_traces_match; eauto.
-    eapply Compile.compile_ts_ok; eauto.
-  Qed.
-
-  Theorem absInitP :
-    forall s1 s2,
-      MailFSStringAbsState.initP s1 ->
-      absR s1 s2 ->
-      MailFSStringAbsState.initP s2.
-  Proof.
-    eauto.
-  Qed.
-
-End MailFSStringImpl.
+Module MailFSStringImpl :=
+  LayerImplMovers
+    MailFSStringAbsState
+    MailFSStringOp MailFSStringAPI
+    MailFSOp MailFSStringAbsAPI.
