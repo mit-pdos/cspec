43349cc784d59432d1731853bdbfa6afe6d766cc
Nickolai Zeldovich <nickolai@csail.mit.edu>
2018-04-06 14:34:24 -0400

    put mailboxes on /dev/shm

--- scripts/mk-users.sh
+++ scripts/mk-users.sh
@@ -2,11 +2,14 @@
 
 NUSER=100
 
+rm -rf /tmp/mailtest
+ln -s /dev/shm/mailtest /tmp/mailtest
+
 echo "Create $NUSER mailboxes"
 for i in `seq 0 $NUSER`;
 do
-    rm -rf /tmp/mailtest/u$i
-    mkdir -p /tmp/mailtest/u$i/mail
-    mkdir -p /tmp/mailtest/u$i/tmp
+    rm -rf /dev/shm/mailtest/u$i
+    mkdir -p /dev/shm/mailtest/u$i/mail
+    mkdir -p /dev/shm/mailtest/u$i/tmp
 done    
 
