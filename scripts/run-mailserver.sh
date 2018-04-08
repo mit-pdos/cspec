2768c2b3ad06b9005fdc8d14ff0a67f854665024
Frans Kaashoek <kaashoek@mit.edu>
2018-04-08 10:45:24 -0400

    x

--- scripts/run-mailserver.sh
+++ scripts/run-mailserver.sh
@@ -1,7 +1,7 @@
 NPROC=$1
 NMSG=100000
 N=$(($NMSG * $NPROC))
-echo "== mail-test $NPROC $NMSG $N == "
+echo "== mail-test $NPROC $NMSG $N `date` == "
 for i in `seq 1 $NPROC`;
 do
     echo "== mail-test $i $((N / i))"
