1e17ed5a74168775551f433f034f4f7df21b4279
Frans Kaashoek <kaashoek@mit.edu>
2018-04-09 19:26:36 -0400

    Oops, run 1 round of pop too.

--- scripts/run-mailserver.sh
+++ scripts/run-mailserver.sh
@@ -7,7 +7,7 @@ echo "== mail-test $NPROC $NMSG $N `date` == "
 for i in `seq 1 $NPROC`;
 do
     echo "== mail-test $i $((N / i))"
-    ./scripts/mk-users.sh; time ./bin/mail-test $NPROC $((N / i)) 1 0
-    sleep 10
+    ./scripts/mk-users.sh; time ./bin/mail-test $NPROC $((N / i)) 1 1
+    sleep 1
 done    
 
