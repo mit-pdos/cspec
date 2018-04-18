5644ffa988228834dd2ca95e9edaf52bc6a2269d
Nickolai Zeldovich <nickolai@csail.mit.edu>
2018-04-17 22:22:38 -0400

    fully decompose MailFSMergedImpl.absR_ok into (mostly admitted) lemmas

--- src/Mail2/MailFSMergedImpl.v
+++ src/Mail2/MailFSMergedImpl.v
@@ -33,29 +33,112 @@ Module MailFSMergedAbsImpl' <:
   Hint Resolve FMap.mapsto_add_ne'.
   Hint Constructors MailFSPathAPI.xstep.
 
-  Theorem absR_ok :
-    op_abs absR MailFSMergedAbsAPI.step MailFSPathHAPI.step.
+  Theorem absR_fs_add :
+    forall fs lock s2 u dir fn data P,
+      absR (MailFSMergedState.mk_state fs lock) s2 ->
+      absR (MailFSMergedState.mk_state (FMap.add (u, dir, fn) data fs) lock)
+           (hadd (exist _ u P)
+                 (MailFSPathAbsState.mk_state
+                   (FMap.add (dir, fn) data
+                     (MailFSPathAbsState.fs (hget s2 (exist _ u P))))
+                   (MailFSPathAbsState.locked (hget s2 (exist _ u P)))) s2).
   Proof.
-    unfold op_abs; intros.
-    unfold MailFSPathHAPI.step.
-    unfold MailFSPathAPI.step.
-    inversion H0; clear H0; subst; repeat sigT_eq.
+    unfold absR; simpl; intros.
+    specialize (H u0); intuition idtac.
+  Admitted.
 
-    all: eexists; split;
-      [
-      | econstructor;
-        match goal with
-        | |- MailFSPathAPI.xstep _ _ ?s _ _ _ =>
-          replace s with (MailFSPathAbsState.mk_state
-            (MailFSPathAbsState.fs s)
-            (MailFSPathAbsState.locked s)) by ( destruct s; reflexivity )
-        | _ => idtac
-        end;
-        try econstructor
-      ].
+  Theorem absR_fs_remove :
+    forall fs lock s2 u dir fn P,
+      absR (MailFSMergedState.mk_state fs lock) s2 ->
+      absR (MailFSMergedState.mk_state (FMap.remove (u, dir, fn) fs) lock)
+           (hadd (exist _ u P)
+                 (MailFSPathAbsState.mk_state
+                   (FMap.remove (dir, fn)
+                     (MailFSPathAbsState.fs (hget s2 (exist _ u P))))
+                   (MailFSPathAbsState.locked (hget s2 (exist _ u P)))) s2).
+  Proof.
+    unfold absR; simpl; intros.
+    specialize (H u0); intuition idtac.
+  Admitted.
 
-    all: eauto.
-    all: try match goal with
+  Theorem absR_MapsTo :
+    forall fs lock s2 u dir fn data P,
+      absR (MailFSMergedState.mk_state fs lock) s2 ->
+      FMap.MapsTo (u, dir, fn) data fs ->
+      FMap.MapsTo (dir, fn) data
+        (MailFSPathAbsState.fs (hget s2 (exist _ u P))).
+  Admitted.
+
+  Theorem absR_In :
+    forall fs lock s2 u dir fn P,
+      absR (MailFSMergedState.mk_state fs lock) s2 ->
+      (~ FMap.In (u, dir, fn) fs) ->
+      ~ FMap.In (dir, fn)
+        (MailFSPathAbsState.fs (hget s2 (exist _ u P))).
+  Admitted.
+
+  Theorem absR_is_permutation_key :
+    forall fs lock s2 u dir r P,
+      absR (MailFSMergedState.mk_state fs lock) s2 ->
+      FMap.is_permutation_key r (drop_dirname (filter_dir (u, dir) fs)) ->
+      FMap.is_permutation_key r (MailFSPathAbsAPI.drop_dirname
+        (MailFSPathAbsAPI.filter_dir dir
+          (MailFSPathAbsState.fs
+            (hget s2 (exist _ u P))))).
+  Admitted.
+
+  Theorem absR_locked :
+    forall fs locked s2 u v P,
+      absR (MailFSMergedState.mk_state fs locked) s2 ->
+      FMap.MapsTo u v locked ->
+      MailFSPathAbsState.locked (hget s2 (exist _ u P)) = v.
+  Admitted.
+
+  Theorem absR_locked_add :
+    forall fs locked s2 u v P,
+      absR (MailFSMergedState.mk_state fs locked) s2 ->
+      absR (MailFSMergedState.mk_state fs (FMap.add u v locked))
+           (hadd (exist _ u P)
+                 (MailFSPathAbsState.mk_state
+                   (MailFSPathAbsState.fs (hget s2 (exist _ u P)))
+                   v) s2).
+  Proof.
+    unfold absR; simpl; intros.
+    specialize (H u0); intuition idtac.
+  Admitted.
+
+  Theorem absR_valid_in_locked : forall fs locked s2 u,
+    absR (MailFSMergedState.mk_state fs locked) s2 ->
+    UserIdx.indexValid u ->
+    FMap.In u locked.
+  Proof.
+    destruct s2; intros.
+    eapply HSValid in H0.
+    eapply FMap.in_mapsto_exists in H0; deex.
+    specialize (H u); simpl in *; intuition idtac.
+    eapply H1 in H0; intuition idtac.
+    eapply FMap.mapsto_in; eauto.
+  Qed.
+
+  Hint Resolve absR_fs_add.
+  Hint Resolve absR_fs_remove.
+  Hint Resolve absR_MapsTo.
+  Hint Resolve absR_In.
+  Hint Resolve absR_is_permutation_key.
+  Hint Resolve absR_locked_add.
+  Hint Resolve absR_valid_in_locked.
+
+  Ltac split_path_abs_state :=
+    match goal with
+    | |- MailFSPathAPI.xstep _ _ ?s _ _ _ =>
+      replace s with (MailFSPathAbsState.mk_state
+        (MailFSPathAbsState.fs s)
+        (MailFSPathAbsState.locked s)) by ( destruct s; reflexivity )
+    | _ => idtac
+    end.
+
+  Ltac nop_hadd_path_abs_state :=
+    match goal with
     | |- absR _ (hadd _ (MailFSPathAbsState.mk_state
                           (MailFSPathAbsState.fs ?s)
                           (MailFSPathAbsState.locked ?s)) _) =>
@@ -66,7 +149,38 @@ Module MailFSMergedAbsImpl' <:
       eassumption
     end.
 
-  Admitted.
+  Theorem absR_ok :
+    op_abs absR MailFSMergedAbsAPI.step MailFSPathHAPI.step.
+  Proof.
+    unfold op_abs; intros.
+    unfold MailFSPathHAPI.step.
+    unfold MailFSPathAPI.step.
+    inversion H0; clear H0; subst; repeat sigT_eq.
+
+    all: eexists.
+    all: try solve [ split;
+      [
+      | econstructor;
+        split_path_abs_state;
+        try erewrite absR_locked by eauto;
+        econstructor;
+        eauto
+      ];
+      eauto
+    ].
+
+    all: try solve [ split;
+      [ eassumption
+      | rewrite <- hadd_hget_eq with (H0 := P) (H1 := P) at 2;
+        econstructor;
+        split_path_abs_state;
+        econstructor;
+        eauto
+      ]
+    ].
+
+    all: try solve [ split; [ eauto | exfalso; eauto ] ].
+  Qed.
 
   Theorem absInitP :
     forall s1 s2,
