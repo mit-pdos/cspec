dc0abb941fad6fdadbf44cded599ed9a96b6d750
Nickolai Zeldovich <nickolai@csail.mit.edu>
2018-08-30 13:38:43 -0400

    script to run gomail benchmark

---
+++ scripts/run-gomail.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+NPROC=$1
+NMSG=100000
+N=$(($NMSG * $NPROC))
+TIMEFORMAT='real %R nuser %U sys %S (s)'
+echo "== gomail $NPROC $NMSG $N `date` == "
+for i in `seq 1 $NPROC`;
+do
+    echo "== gomail $i $((N / i))"
+    ./scripts/mk-users.sh; time ( cd ./gomail/msrv && GOMAIL_NPROC=$i GOMAIL_NITER=$((N / i)) go test -v )
+    sleep 1
+done    
+
