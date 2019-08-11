0c1424415629742a0515b3ac1d3b28384fcac890
Tej Chajed <tchajed@mit.edu>
2019-08-11 17:23:14 -0400

    Make scripts more robust
    - runnable from any directory
    - pass shellcheck

--- scripts/run-gomail.sh
+++ scripts/run-gomail.sh
@@ -1,11 +1,15 @@
 #!/bin/bash
 
+set -e
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+cd "$DIR/.."
 NPROC=$1
 NMSG=100000
-N=$(($NMSG * $NPROC))
+N=$((NMSG * NPROC))
 TIMEFORMAT='real %R nuser %U sys %S (s)'
-echo "== gomail $NPROC $NMSG $N `date` == "
-for i in `seq 1 $NPROC`;
+echo "== gomail $NPROC $NMSG $N $(date) == "
+for i in $(seq 1 "$NPROC")
 do
     echo "== gomail $i $((N / i))"
     ./scripts/mk-users.sh; time ( cd ./gomail/msrv && GOMAIL_NPROC=$i GOMAIL_NITER=$((N / i)) go test -v )
--- scripts/run-mailserver.sh
+++ scripts/run-mailserver.sh
@@ -1,14 +1,18 @@
 #!/bin/bash
 
+set -e
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+cd "$DIR/.."
 NPROC=$1
 NMSG=100000
-N=$(($NMSG * $NPROC))
+N=$((NMSG * NPROC))
 TIMEFORMAT='real %R nuser %U sys %S (s)'
-echo "== mail-test $NPROC $NMSG $N `date` == "
-for i in `seq 1 $NPROC`;
+echo "== mail-test $NPROC $NMSG $N $(date) == "
+for i in $(seq 1 "$NPROC")
 do
     echo "== mail-test $i $((N / i))"
-    ./scripts/mk-users.sh; time ./bin/mail-test $i $((N / i)) 1 1
+    ./scripts/mk-users.sh; time ./bin/mail-test "$i" "$((N / i))" 1 1
     sleep 1
 done    
 
